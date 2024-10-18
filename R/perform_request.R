e20eecab32acc99f7d93a192d8b933118f9b210e
Christopher Carroll Smith <chriscarrollsmith@gmail.com>
2024-10-18 17:11:33 -0400

    Adds renv and lintr and fixes all lint errors

--- R/perform_request.R
+++ R/perform_request.R
@@ -1,75 +1,108 @@
-
 #' @keywords internal
 #'
 perform_request <- function(
-    resource,
-    per_page = 1000,
-    date = NULL,
-    progress = FALSE,
-    base_url = "https://api.worldbank.org/v2/sources/6/"
+  resource,
+  per_page = 1000,
+  date = NULL,
+  progress = FALSE,
+  base_url = "https://api.worldbank.org/v2/sources/6/"
 ) {
+  validate_per_page(per_page)
 
-  if (!is.numeric(per_page) || per_page %% 1L != 0 || per_page < 1L || per_page > 32500L) {
-    cli::cli_abort("{.arg per_page} must be an integer between 1 and 32,500.")
-  }
-
-  is_request_error <- function(resp) {
-    status <- resp_status(resp)
-    if (status >= 400L) {
-      return(TRUE)
-    }
-    body <- resp_body_json(resp)
-    if (length(body) == 1L && length(body[[1L]]$message) == 1L) {
-      return(TRUE)
-    }
-    FALSE
-  }
-
-  check_for_body_error <- function(resp) {
-    content_type <- resp_content_type(resp)
-    if (identical(content_type, "application/json")) {
-      body <- resp_body_json(resp)
-      message_id <- body[[1]]$message[[1]]$id
-      message_value <- body[[1]]$message[[1]]$value
-      error_code <- paste("Error code:", message_id)
-      docs <- "Read more at <https://datahelpdesk.worldbank.org/knowledgebase/articles/898620-api-error-codes>"
-      c(error_code, message_value, docs)
-    }
-  }
-
-  req <- request(base_url) |>
-    req_url_path_append(resource) |>
-    req_url_query(format = "json", per_page = per_page, date = date) |>
-    req_user_agent("wbids R package (https://github.com/teal-insights/r-wbids)")
-
-  resp <- req_perform(req)
+  req <- create_request(base_url, resource, per_page, date)
+  resp <- httr2::req_perform(req)
 
   if (is_request_error(resp)) {
-    error_body <- check_for_body_error(resp)
+    handle_request_error(resp)
   }
 
-  body <- resp_body_json(resp)
-
-  if (length(body) == 2) {
-    pages <- body[[1L]]$pages
-  } else {
-    pages <- body$pages
-  }
+  body <- httr2::resp_body_json(resp)
+  pages <- extract_pages(body)
 
   if (pages == 1L) {
-    if (length(body) == 2) {
-      out <- body[[2L]]
-    } else {
-      out <- body$source
-    }
+    out <- extract_single_page_data(body)
   } else {
-    resps <- req |>
-      req_perform_iterative(next_req = iterate_with_offset("page"),
-                            max_reqs = pages,
-                            progress = progress)
-    out <- resps |>
-      map(function(x) {resp_body_json(x)[[2]]})
-    out <- unlist(out, recursive = FALSE)
+    out <- fetch_multiple_pages(req, pages, progress)
   }
   out
+}
+
+validate_per_page <- function(per_page) {
+  if (
+    !is.numeric(per_page) || per_page %% 1L != 0 ||
+      per_page < 1L || per_page > 32500L
+  ) {
+    cli::cli_abort("{.arg per_page} must be an integer between 1 and 32,500.")
+  }
+}
+
+create_request <- function(base_url, resource, per_page, date) {
+  httr2::request(base_url) |>
+    httr2::req_url_path_append(resource) |>
+    httr2::req_url_query(format = "json", per_page = per_page, date = date) |>
+    httr2::req_user_agent(
+      "wbids R package (https://github.com/teal-insights/r-wbids)"
+    )
+}
+
+is_request_error <- function(resp) {
+  status <- httr2::resp_status(resp)
+  if (status >= 400L) {
+    return(TRUE)
+  }
+  body <- httr2::resp_body_json(resp)
+  if (length(body) == 1L && length(body[[1L]]$message) == 1L) {
+    return(TRUE)
+  }
+  FALSE
+}
+
+handle_request_error <- function(resp) {
+  error_body <- check_for_body_error(resp)
+  cli::cli_alert_danger(paste(error_body, collapse = "\n"))
+}
+
+check_for_body_error <- function(resp) {
+  content_type <- httr2::resp_content_type(resp)
+  if (identical(content_type, "application/json")) {
+    body <- httr2::resp_body_json(resp)
+    message_id <- body[[1]]$message[[1]]$id
+    message_value <- body[[1]]$message[[1]]$value
+    error_code <- paste("Error code:", message_id)
+    docs <- paste0(
+      "Read more at <https://datahelpdesk.worldbank.org/",
+      "knowledgebase/articles/898620-api-error-codes>"
+    )
+    c(error_code, message_value, docs)
+  }
+}
+
+extract_pages <- function(body) {
+  if (length(body) == 2) {
+    body[[1L]]$pages
+  } else {
+    body$pages
+  }
+}
+
+extract_single_page_data <- function(body) {
+  if (length(body) == 2) {
+    body[[2L]]
+  } else {
+    body$source
+  }
+}
+
+fetch_multiple_pages <- function(req, pages, progress) {
+  resps <- req |>
+    httr2::req_perform_iterative(
+      next_req = httr2::iterate_with_offset("page"),
+      max_reqs = pages,
+      progress = progress
+    )
+  out <- resps |>
+    purrr::map(function(x) {
+      httr2::resp_body_json(x)[[2]]
+    })
+  unlist(out, recursive = FALSE)
 }
