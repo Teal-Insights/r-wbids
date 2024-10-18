73e0f591788ba4aa36edbaf8ae796930c0d61924
Christoph Scheuch <19249734+christophscheuch@users.noreply.github.com>
2024-10-18 17:58:12 -0400

    Create global-variables.R

---
+++ R/global-variables.R
@@ -0,0 +1,5 @@
+# We only use system data objects as global variables.
+# See https://cran.r-project.org/web/packages/dplyr/vignettes/in-packages.html
+# For data masking, import .data from rlang and then use .data$var instead of var.
+# For tidy selection, use "var" instead of var.
+utils::globalVariables(c("series", "series_topics"))
