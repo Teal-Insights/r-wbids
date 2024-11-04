2645798f8c188d3b08cf769715f29209208bafe9
Christoph Scheuch <19249734+christophscheuch@users.noreply.github.com>
2024-11-04 19:37:14 +0100

    Add readxl as suggested import

--- R/ids_bulk.R
+++ R/ids_bulk.R
@@ -1,5 +1,9 @@
 ids_bulk <- function(file_url, file_path = tempfile(fileext = ".xlsx")) {
 
+  rlang::check_installed(
+    "readxl", reason = "to download bulk files."
+  )
+
   cli::cli_inform("Downloading file to: {file_path}")
   utils::download.file(file_url, destfile = file_path, quiet = TRUE)
 
