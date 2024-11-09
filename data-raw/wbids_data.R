04d37841889fdee7bf9eecb16b61cb3d7a21e271
Christoph Scheuch <19249734+christophscheuch@users.noreply.github.com>
2024-11-09 14:51:06 +0100

    Tie in econid package for geographies and counterparts

--- data-raw/wbids_data.R
+++ data-raw/wbids_data.R
@@ -1,28 +1,15 @@
-
+library(dplyr) 
 library(wbwdi)
-library(tidyr)
-library(usethis)
-library(devtools)
 library(econid)
+library(devtools)
 
 load_all()
 
 # Prepare geography data -------------------------------------------------------
 
-geographies_raw <- perform_request("country")
-
-geographies_ids <- geographies_raw[[1]]$concept[[1]]$variable |>
-  bind_rows() |>
-  select(geography_id = id,
-         geography_name = value)
-
-geographies_wdi <- wdi_get_geographies()
-
-# TODO: geographies_name needs to be enriched with micropackage later
-geographies <- geographies_ids |>
-  left_join(geographies_wdi |>
-              select(-c(geography_name, longitude, latitude)),
-            join_by(geography_id))
+# TODO: once econid is on CRAN, directly call this in id_list_geographies() 
+#       and add econid to Imports
+geographies <- econid::geographies
 
 # Prepare series data ----------------------------------------------------------
 
@@ -46,52 +33,9 @@ series <- series_extended |>
 
 # Prepare counterpart data -----------------------------------------------------
 
-counterparts_raw <- perform_request("counterpart-area")
-
-counterparts_ids <- counterparts_raw[[1]]$concept[[1]]$variable |>
-  bind_rows() |>
-  select(counterpart_id = id,
-         counterpart_name = value)
-
-global_ifis <- c(
-  "International Monetary Fund",
-  "Bank for International Settlements (BIS)"
-)
-
-global_mdbs <- c(
-  "World Bank-IDA",
-  "World Bank-IBRD",
-  "World Bank-MIGA",
-  "International Finance Corporation",
-  "International Fund for Agricultural Dev.",
-  "European Bank for Reconstruction and Dev. (EBRD)",
-  "African Dev. Bank",
-  "Asian Dev. Bank",
-  "Inter-American Dev. Bank",
-  "Asian Infrastructure Investment Bank"
-)
-
-# TODO: counterpart data needs to be updated with geography micropackage later
-
-counterparts <- counterparts_ids |>
-  left_join(geographies_wdi |>
-              select(geography_name,
-                     counterpart_iso2code = geography_iso2code,
-                     counterpart_iso3code = geography_id,
-                     geography_type),
-            join_by(counterpart_name == geography_name)) |>
-  mutate(
-    counterpart_type = case_when(
-      counterpart_name %in% global_ifis ~ "Global IFIs",
-      counterpart_name %in% global_mdbs ~ "Global MDBs",
-      counterpart_name %in% c("Bondholders") ~ "Bondholders",
-      counterpart_name %in% c("World") ~ "All Creditors",
-      geography_type == "Country" ~ "Country",
-      geography_type == "Region" ~ "Region",
-      .default = "Other"
-    )
-  ) |>
-  select(-geography_type)
+# TODO: once econid is on CRAN, directly call this in id_list_geographies() 
+#       and add econid to Imports
+counterparts <- econid::counterparts
 
 # Prepare series-topics data ---------------------------------------------------
 
@@ -101,7 +45,7 @@ series_topics <- series_extended |>
 
 # Store all data in single rda file --------------------------------------------
 
-usethis::use_data(
+use_data(
   geographies, series, counterparts, series_topics,
   overwrite = TRUE, internal = TRUE
 )
